8a7695ea53eff85b25ca791d1258283539995055
Luis Carrillo Gutiérrez <carrilloluis@users.noreply.github.com>
2022-08-26 21:55:52 -0500

    Add files via upload

---
+++ fillEllipse2D/main.lua
@@ -0,0 +1,15 @@
+-- fillellipse.sdl.bas
+--[[
+setdisplay(320, 240, 32, 1)
+paper(0xFFFFFF):ink(0x000000):pen(0x000000):cls
+fillellipse(120, 100, 90, 60)
+waitkey
+--]]
+function love.load()
+	love.graphics.setBackgroundColor(1, 1, 1, 1)
+	love.graphics.setColor(0, 0, 0)
+end
+
+function love.draw()
+	love.graphics.ellipse("fill", 120, 100, 90, 60)
+end
